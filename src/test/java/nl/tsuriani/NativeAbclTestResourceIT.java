c4ce76b91b0ff43149bcc04201aceaeeba33571a
Tristano Suriani <tristano.suriani@ordina.nl>
2020-11-23 13:22:10 +0100

    removed unnecessary tests

--- src/test/java/nl/tsuriani/NativeAbclTestResourceIT.java
+++
@@ -1,9 +0,0 @@
-package nl.tsuriani;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeAbclTestResourceIT extends AbclTestResourceTest {
-
-    // Execute the same tests but in native mode.
-}
